7abc60793b0a84775b6d8a21b2d78e6486469718
Jacob Roth <jakeroth0@gmail.com>
2023-04-16 22:35:12 -0400

    added signup functionality

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -38,7 +38,9 @@ const typeDefs = gql`
 
   type Mutation {
     login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
   }
+  
 `;
 
 module.exports = typeDefs;
